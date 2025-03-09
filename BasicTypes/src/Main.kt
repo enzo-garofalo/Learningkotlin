cacca25371a434d718bd2658220cda44c715475a
enzo-garofalo <dev.enzo.garofalo@gmail.com>
2025-03-09 09:26:52 -0300

    Basic

---
+++ BasicTypes/src/Main.kt
@@ -0,0 +1,39 @@
+fun main()
+{
+    // Operators
+    var iceCream = 10
+
+    iceCream = iceCream + 3
+    println(iceCream)
+
+    iceCream += 7
+    println(iceCream)
+
+    iceCream -= 7
+    println(iceCream)
+
+    iceCream *= 2
+    println(iceCream)
+
+    iceCream /= 2
+    println(iceCream)
+
+    // Types
+    var int         :Int = 2025
+    var unsignedInt :UInt = 100u
+    var float       :Float = 3.14f
+    var double      :Double = 3.14
+    var boolean     :Boolean = true
+    var char        :Char = 'a'
+    var byte        :Byte = 127
+    var string      :String = "Hello"
+
+    // Exercise
+    val a :Int = 10000
+    val b :String = "abc"
+    val c :Double = 3.14
+    val d :Long = 100_000_000_000
+    val e :Boolean = true
+    val f :Char = '\n'
+
+}
---
+++ HelloWorld/src/Main.kt
@@ -0,0 +1,21 @@
+
+// Learning about Variables
+
+fun main() {
+    println("Eita mundo bom")
+    val popcorn = 5
+    val hotdog = 7
+    var customers = 10
+    customers = 8
+
+    //println(customers)
+
+    // String template
+
+    println("There are $customers customers")
+    println("There are ${customers+1} customers")
+
+    val name:String = "Andressa"
+    var age = 26
+    println("$name is $age years old")
+}
